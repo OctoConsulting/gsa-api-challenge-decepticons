55cb067cd58441721a9f5d34e95407ebcf1a6296
Andrew Tran <trandrew08@gmail.com>
2020-07-22 10:45:13 -0500

    Adding Geochart and api calls

--- src/app/dashboard/map/map.component.ts
+++ src/app/dashboard/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api-service.service';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-
-  constructor() { }
+  public geochartData = [];
+  public loading = true;
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.fetchData();
+  }
+
+  private fetchData(): void {
+    this.apiService.getOppCountsByGeoData({}).subscribe(
+      response => {
+        this.geochartData = response;
+        this.loading = false;
+      },
+      error => console.log(error)
+    );
+  }
+
+  public geochartClicked(event: any): void {
+    console.log('State clicked: ' + event.key);
   }
 
 }
--- src/app/dashboard/services/api-service.service.ts
+++ src/app/dashboard/services/api-service.service.ts
@@ -6,4 +6,24 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
+  private readonly baseUrl = 'http://127.0.0.1:3000';
+  private readonly apiKey = 'zAdl8SNdzc1Y06aCX6JN1nXIqT060ejvG0LJbDKK';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getOppByStatus(queryParams: any): Observable<any> {
+    return this.httpClient.get(`${this.baseUrl}/status`, {params: queryParams});
+  }
+
+  getOppTypesByStatus(queryParams: any): Observable<any> {
+    return this.httpClient.get(`${this.baseUrl}/type`, {params: queryParams});
+  }
+
+  getOppCountsByConsumer(queryParams: any): Observable<any> {
+    return this.httpClient.get(`${this.baseUrl}/consumer`, {params: queryParams});
+  }
+
+  getOppCountsByGeoData(queryParams: any): Observable<any> {
+    return this.httpClient.get(`${this.baseUrl}/map`, {params: queryParams});
+  }
 }
