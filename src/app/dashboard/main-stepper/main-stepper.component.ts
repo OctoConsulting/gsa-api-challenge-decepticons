8a6b5034decb735acfb5600e65e0ca1390803c5a
Andrew Tran <trandrew08@gmail.com>
2020-07-22 11:03:33 -0500

    Adding tables and stepper

--- src/app/dashboard/main-stepper/main-stepper.component.ts
+++ src/app/dashboard/main-stepper/main-stepper.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit, Output, EventEmitter } from '@angular/core';
+import { MatStepper } from '@angular/material/stepper';
+import { FileSaverService } from '../services/file-saver.service';
 
 @Component({
   selector: 'app-main-stepper',
   templateUrl: './main-stepper.component.html',
   styleUrls: ['./main-stepper.component.scss']
 })
-export class MainStepperComponent implements OnInit {
+export class MainStepperComponent implements OnInit, AfterViewInit {
+  @Input() piechart1Data;
+  @Input() piechart2Data;
+  @Input() barchart1Data;
 
-  constructor() { }
+  @Output() chart1Event = new EventEmitter();
+
+  @ViewChild('stepper') stepper: MatStepper;
+  constructor(public fileSaverService: FileSaverService) { }
 
   ngOnInit(): void {
+    // this.stepper.selectedIndex = 1;
+  }
+
+  ngAfterViewInit(): void {
+    // this.stepper.selectedIndex = 1;
+  }
+
+  public onChart1Click(event: any): void {
+    this.chart1Event.emit(event);
+    this.stepper.next();
+  }
+
+  public exportData(headers: any, data: any): void {
+    this.fileSaverService.saveAsCSV(headers, data);
   }
 
 }
